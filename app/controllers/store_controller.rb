5409a0990ccd3cd5681cc3160092650594d4816a
Brandon Smith <brandon.smith@yale.edu>
2013-05-02 17:58:20 -0400

    added a bunch of cart-related stuff

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,12 @@
 class StoreController < ApplicationController
   def index
   	@products = Product.order(:title)
+  	@count = increment_count 
+  	@msg = "You've been here #{@count} times" if @count > 5
   end
+
+  def increment_count
+	  session[:counter] ||= 0
+	  session[:counter] += 1
+	end
 end
