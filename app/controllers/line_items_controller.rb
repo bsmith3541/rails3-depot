e4350823a218d22a527211f7d860687d51045a04
Brandon Smith <brandon.smith@yale.edu>
2013-05-14 17:33:43 -0500

    added AJAX-y components

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -33,6 +33,8 @@ class LineItemsController < ApplicationController
     respond_to do |format|
       if @line_item.save
         format.html { redirect_to store_url }
+        # format.js is for the AJAX implementation
+        format.js { @current_item = @line_item }
         format.json { render action: 'show', status: :created, location: @line_item }
       else
         format.html { render action: 'new' }
