38e7c9b865111a39d22b9e8c8d516bb131002b9d
Brandon Smith <brandon.smith@yale.edu>
2013-05-16 09:27:49 -0500

    added email functionality with integration tests

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,6 +16,11 @@ Depot::Application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
+  # Configure how email is sent via Gmail
+  config.action_mailer.delivery_method = :test
+
+  
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
